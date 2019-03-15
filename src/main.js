dd72a80b589a162ef1700c8fc570fa0c9f082c9d
Sungho Kim <shirenbeat@gmail.com>
2019-03-15 18:28:23 +0900

    04

--- src/main.js
+++ src/main.js
@@ -1,35 +1,45 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 
 import App from './App.vue';
 import store from './store';
 
-new Vue({
-  strict: true,
-  store,
-  render: h => h(App)
-}).$mount('#app');
-
-
-// const counterStore = new Vuex.Store({
-//   strict: true,
-//   state: {
-//     count: 1
-//   }
-// });
-
-// const Counter = {
-//   template: '<div>{{ count}}</div>',
-//   computed: {
-//     count() {
-//       return this.$store.state.count;
-//     }
-//   }
-// }
-
 // new Vue({
 //   strict: true,
-//   store: counterStore,
-//   render: h => h(Counter)
+//   store,
+//   render: h => h(App)
 // }).$mount('#app');
 
-// window.cs = counterStore;
+
+const counterStore = new Vuex.Store({
+  strict: true,
+  state: {
+    count: 1,
+    createdTime: new Date()
+  },
+  getters: {
+    detail: state => {
+      return `createdTime: ${state.createdTime}`;
+    }
+  }
+});
+
+const Counter = {
+  template: '<div>{{count}}<p>{{detail}}</p></div>',
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    detail() {
+      return this.$store.getters.detail;
+    }
+  }
+}
+
+new Vue({
+  strict: true,
+  store: counterStore,
+  render: h => h(Counter)
+}).$mount('#app');
+
+window.cs = counterStore;
