fdbc4fddd6e1ad5e1ccbb3c2d9b036b390b7ec28
Sungho Kim <shirenbeat@gmail.com>
2019-03-15 18:03:20 +0900

    02

--- src/main.js
+++ src/main.js
@@ -5,12 +5,37 @@ import App from './App.vue';
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
-  state: {}
+// const store = new Vuex.Store({
+//   state: {}
+// });
+
+// new Vue({
+//   strict: true,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app');
+
+
+const counterStore = new Vuex.Store({
+  strict: true,
+  state: {
+    count: 1
+  }
 });
+
+const Counter = {
+  template: '<div>{{ count}}</div>',
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+}
 
 new Vue({
   strict: true,
-  store,
-  render: h => h(App)
+  store: counterStore,
+  render: h => h(Counter)
 }).$mount('#app');
+
+window.cs = counterStore;
