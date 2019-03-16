f847ff5ab969f3f0e4ed4d051625af5bb8ee8055
Sungho Kim <shirenbeat@gmail.com>
2019-03-16 10:18:50 +0900

    07

--- src/main.js
+++ src/main.js
@@ -1,44 +1,50 @@
 import Vue from 'vue';
 
-import App from './App.vue';
-import store from './store';
-
-new Vue({
-  strict: true,
-  store,
-  render: h => h(App)
-}).$mount('#app');
-
-// import Vuex from 'vuex';
-// const counterStore = new Vuex.Store({
-//   strict: true,
-//   state: {
-//     count: 1,
-//     createdTime: new Date()
-//   },
-//   getters: {
-//     detail: state => {
-//       return `createdTime: ${state.createdTime}`;
-//     }
-//   }
-// });
-
-// const Counter = {
-//   template: '<div>{{count}}<p>{{detail}}</p></div>',
-//   computed: {
-//     count() {
-//       return this.$store.state.count;
-//     },
-//     detail() {
-//       return this.$store.getters.detail;
-//     }
-//   }
-// }
+// import App from './App.vue';
+// import store from './store';
 
 // new Vue({
 //   strict: true,
-//   store: counterStore,
-//   render: h => h(Counter)
+//   store,
+//   render: h => h(App)
 // }).$mount('#app');
 
-// window.cs = counterStore;
+import Vuex from 'vuex';
+Vue.use(Vuex);
+const counterStore = new Vuex.Store({
+  strict: true,
+  state: {
+    count: 1,
+    createdTime: new Date()
+  },
+  getters: {
+    detail: state => {
+      return `createdTime: ${state.createdTime}`;
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+});
+
+const Counter = {
+  template: '<div>{{count}}<p>{{detail}}</p></div>',
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    detail() {
+      return this.$store.getters.detail;
+    }
+  }
+};
+
+new Vue({
+  strict: true,
+  store: counterStore,
+  render: h => h(Counter)
+}).$mount('#app');
+
+window.cs = counterStore;
