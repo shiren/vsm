fcb26559581a16a4b3e4456659755226db4a47c3
Sungho Kim <shirenbeat@gmail.com>
2019-05-28 13:57:38 +0900

    main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
-
 import App from './App.vue';
-
-Vue.use(Vuex);
-
-const store = new Vuex.Store({
-  state: {}
-});
 
 new Vue({
   strict: true,
-  store,
   render: h => h(App)
 }).$mount('#app');
